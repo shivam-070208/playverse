afae6383d4c3499479b8daf2192f2d44d3f798d3
shvgpt-070208 <shvgpt070208@gmail.com>
2026-01-24 23:04:23 +0530

    bug:prisma-fixed

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,21 @@
-import "dotenv/config"
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "./generated/prisma/client"
-const connectionString = process.env.DATABASE_URL
-const adapter = new PrismaPg({
-    connectionString,
-})
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
-const db = new PrismaClient({adapter})
+const connectionString = process.env.DATABASE_URL;
 
-export {db};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
+}
+export { db };
