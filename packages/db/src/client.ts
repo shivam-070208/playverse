5fc5111180632b7b1a965a143f868b1bf8fc9ddd
shvgpt-070208 <shvgpt070208@gmail.com>
2026-01-31 23:47:25 +0530

    bug:fixed devdependency bug inside backend /prisma unwanted connection fix

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -9,10 +9,13 @@ declare global {
 }
 
 const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const db =
+  globalForPrisma.prisma ??
+  (() => {
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter });
+  })();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db;
